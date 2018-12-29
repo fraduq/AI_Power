a050308e81d135843269a08b1e5beb6020297405
KyrieHee <hyf6127@126.com>
2018-12-29 22:41:54 +0800

    modified py file

--- GridSearch.py
+++ GridSearch.py
@@ -25,3 +25,31 @@ print("----------------------------cv------------------------------")
 print(gbm.cv)
 
 print('Best parameters found by grid search are:', gbm.best_params_)
+
+#=========================================================================
+#随机敲定一组参数跑模型
+import lightgbm as lgb
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+%matplotlib inline
+# create dataset for lightgbm
+lgb_train = lgb.Dataset(X_lgb, y_lgb)
+# specify your configurations as a dict
+params = {
+    'num_leaves': 128,
+    'learning_rate':0.01,
+    'n_estimators':800,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'weight': weights,
+    'application':'regression_l1'
+}
+
+print('Start training...')
+# train
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=800,
+                feature_name=['f' + str(i + 1) for i in range(X_lgb.shape[1])],
+                categorical_feature=[0,1,2,3,4,5])
